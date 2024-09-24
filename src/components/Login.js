b034bed071b4708b8c1b07b6ab3c13617c0d96d2
Shrey Jain <143933598+shreyjain14@users.noreply.github.com>
2024-09-24 07:10:20 +0000

    added forgot password

--- src/components/Login.js
+++ src/components/Login.js
@@ -87,7 +87,11 @@ const Login = ({ setUser }) => {
                   <button className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Login</button>
                 </div>
               </div>
+              
             </form>
+            <p className="mt-2 text-center">
+               <Link to="/forgot-password" className="text-blue-600 hover:underline">Forgot Password?</Link>
+            </p>
             <p className="mt-4 text-center">
               Don't have an account? <Link to="/register" className="text-blue-600 hover:underline">Register</Link>
             </p>
